5dae8f8f9ee5f6b59286525d2a8b1eb0b7c40cc9
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-13 18:46:35 +0000

    Step 1 complete

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+  https.get(requestOptions, function(response){
+
+    //Set encoding to utf-8
+    response.setEncoding('utf-8');
+
+    response.on('data', function(chunk){
+      console.log("%s \n", chunk);
+    });
+  });
+}
+
+getAndPrintHTMLChunks();
