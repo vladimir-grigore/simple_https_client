a63aae71672ee236ae00d209af39a53c77c5fa18
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-13 19:08:32 +0000

    Create getHTML.js file

---
+++ getHTML.js
@@ -0,0 +1,14 @@
+function getHTML (options, callback) {
+
+  /* Add your code here */
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
