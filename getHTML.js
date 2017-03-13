ac572aae1e23fcdfcad9485126336a92464bd9cc
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-13 19:10:27 +0000

    Re-use code from step 3

--- getHTML.js
+++ getHTML.js
@@ -1,6 +1,31 @@
+var https = require('https');
+
+//Create placeholder to store all the response
+var data = '';
+
 function getHTML (options, callback) {
 
-  /* Add your code here */
+  //* Add your code here */
+  https.get(options, function(response){
+
+    //Set encoding to utf-8
+    response.setEncoding('UTF8');
+
+    //Print out the https response
+    response.on('data', function(chunk){
+      data += chunk;
+    });
+
+    //Handle error responses
+    response.on('error', function(err){
+      console.log("Error:", err.stack);
+    });
+
+    //Display all the data when reaching the end of the stream
+    response.on('end', function(){
+      console.log(data);
+    });
+  });
 
 }
 
@@ -12,3 +37,5 @@ var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step4.html'
 };
+
+getHTML(requestOptions, printHTML);
