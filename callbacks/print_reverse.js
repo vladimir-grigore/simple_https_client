158e01944649f19463af125213fc9141717cbacd
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-13 19:44:36 +0000

    Implement print_reverse function

--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -1,1 +1,13 @@
-print_reverse.js
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+function print_reverse (html) {
+  html = html.split("").reverse().join("");
+  console.log(html);
+}
+
+getHTML(requestOptions, print_reverse);
