7fe7c9b2f76b9e1f1fc1eeb247fde641d0d718c0
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-13 19:15:00 +0000

    Export the getHTML function

--- getHTML.js
+++ getHTML.js
@@ -3,7 +3,7 @@ var https = require('https');
 //Create placeholder to store all the response
 var data = '';
 
-function getHTML (options, callback) {
+module.exports = function getHTML (options, callback) {
 
   //* Add your code here */
   https.get(options, function(response){
