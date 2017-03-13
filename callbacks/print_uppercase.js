9588d28963c1598a9a5319d784951574a2a3c36d
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-13 19:41:19 +0000

    Implement print_uppercase function

--- callbacks/print_uppercase.js
+++ callbacks/print_uppercase.js
@@ -1,1 +1,13 @@
-print_uppercase.js
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+function print_uppercase (html) {
+  html = html.toUpperCase();
+  console.log(html);
+}
+
+getHTML(requestOptions, print_uppercase);
