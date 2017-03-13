135ab5927b15af8b0bd65eaec0d45a69bf02de30
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-13 19:42:37 +0000

    Implement print_lowercase function

--- callbacks/print_lowercase.js
+++ callbacks/print_lowercase.js
@@ -1,1 +1,13 @@
-print_lowercase.js
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+function print_lowercase (html) {
+  html = html.toLowerCase();
+  console.log(html);
+}
+
+getHTML(requestOptions, print_lowercase);
