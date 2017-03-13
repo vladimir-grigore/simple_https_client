46ee4c18718c155b5a1db74da0172c87df63a2d6
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-03-13 19:47:48 +0000

    Update paths

--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -2,7 +2,7 @@ var getHTML = require('../http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step5.html'
+  path: '//http-examples/step6/reverse.html'
 };
 
 function print_reverse (html) {
